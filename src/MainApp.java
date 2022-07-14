b35b09dede7af522fa919af9e00b011981465ecd
josepmartorell <martorelljosep@gmail.com>
2022-07-14 10:51:24 +0200

    instancia electrodomestico main

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,10 +1,16 @@
-
+import javax.swing.JOptionPane;
 
 public class MainApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-					
+		
+		// INSTANCIAMOS UN OBJETO TIPO ELECTRODOMÉSTICO MOSTRANDO POR PANTALLA LOS ATRIBUTOS PARAMETRIZADOS Y VALIDADOS EN EL TERCER CONSTRUCTOR
+		Electrodomestico electrodomestico = new Electrodomestico(300, "rojo", "b", 10);
+		JOptionPane.showMessageDialog(null, "PRECIO BASE: "+electrodomestico.getPrecioBase()
+				+"\nCOLOR: "+electrodomestico.getColor()
+				+"\nCONSUMO ENERGÉTICO: "+electrodomestico.getConsumoEnergetico()
+				+"\nPESO: "+electrodomestico.getPeso(), "CLASE ELECTRODOMESTICO", JOptionPane.INFORMATION_MESSAGE);	
 
 		
 	}
