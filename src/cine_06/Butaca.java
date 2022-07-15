fd04c7642fd75cba811eeee8dd32e102cec95b19
joelromero2911 <108723592+joelromero2911@users.noreply.github.com>
2022-07-15 11:07:22 +0200

    add Espectador, Butaca

--- src/cine_06/Butaca.java
+++ src/cine_06/Butaca.java
@@ -1,5 +1,27 @@
 package cine_06;
 
 public class Butaca {
+	private String nombre;
+	private boolean ocupado;
+	
+	public Butaca(String nombre, boolean ocupado) {
+		this.nombre = nombre;
+		this.ocupado = ocupado;
+	}
 
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public boolean isOcupado() {
+		return ocupado;
+	}
+
+	public void setOcupado(boolean ocupado) {
+		this.ocupado = ocupado;
+	}
 }
--- src/cine_06/Espectador.java
+++ src/cine_06/Espectador.java
@@ -1,5 +1,57 @@
 package cine_06;
 
 public class Espectador {
+	
+	private String nombre;
+	private int edad;
+	private float dinero;
+	private String butacaAsignada;
+	
+	public Espectador() {
+		
+		this.nombre = "Pepe";
+		this.edad = 18;
+		this.dinero = 10F;
+		this.butacaAsignada = "";
+	}
 
+	public Espectador(String nombre, int edad, float dinero, String butacaAsignada) {
+		
+		this.nombre = nombre;
+		this.edad = edad;
+		this.dinero = dinero;
+		this.butacaAsignada = butacaAsignada;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public float getDinero() {
+		return dinero;
+	}
+
+	public void setDinero(float dinero) {
+		this.dinero = dinero;
+	}
+
+	public String getButacaAsignada() {
+		return butacaAsignada;
+	}
+
+	public void setButacaAsignada(String butacaAsignada) {
+		this.butacaAsignada = butacaAsignada;
+	}	
 }
