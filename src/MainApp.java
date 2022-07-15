2b6b7c7da32d5d6434e9a51c2577e2ab15571700
josepmartorell <martorelljosep@gmail.com>
2022-07-16 00:18:26 +0200

    refactorize main

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,15 +1,6 @@
-import java.security.SecureRandom;
-import java.util.Iterator;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
 import javax.swing.JOptionPane;
 
-import cine_06.Cine;
-import cine_06.Espectador;
 import cine_06.Pelicula;
-import cine_06.Butaca;
-
 import electrodomestico_01.Lavadora;
 
 public class MainApp {
@@ -54,41 +45,83 @@ public class MainApp {
 				// TODO
 				break;
 			case "6":
-				// TODO
-			
-				Pelicula pelicula = new Pelicula("El club de la lucha", 90, 18, "David Fincher");
-				JOptionPane.showMessageDialog(null, "TITULO: "+pelicula.getTitulo()
-				+"\nDURACION: "+pelicula.getDuracion()
-				+"\nEDAD MINIMA: "+pelicula.getEdadMinima()
-				+"\nDIRECTOR: "+pelicula.getDirector()
-				+"\n", "CINE", JOptionPane.INFORMATION_MESSAGE);
-
-				Cine cine = new Cine(pelicula, true);
-				int numEspectadores = Integer.parseInt(JOptionPane.showInputDialog(
-						"Introduce el numero de espectadores que iran a ver la pelicula de culto EL CLUB DE LA LUCHA (max. aforo 72):"));
-				cine.crearEspectadoresAleatorios(numEspectadores, cine);				
-				cine.asignarEspectadoresAleatoriamente();
-				
-				
-				// MOSTRAR ESPECTADORE, SOLO ES UNA PRUEBA, ESTA MAL
-				// ----------------------------------------------------------
-				
-				Iterator<Espectador> it = cine.getEspectadores().iterator();
-		        
-				int cont = 1;
-		        while(it.hasNext()){
-		            Espectador e = it.next();
-		            if(e.getButacaAsignada()!="") {
-		            System.out.println(cont + ". La persona: " + e.getNombre() + " | butaca: " + e.getButacaAsignada());
-		            cont++;}
-		        }
-		        // ----------------------------------------------------------
-		        
+				salaProyeccion();
 				break;
 			default:
 				JOptionPane.showMessageDialog(null, "Opción incorrecta");
 			}
 
 		} while (!opcion.equals("0"));
+		
+		
 	}
+	
+	public static void salaProyeccion() {
+		
+		Pelicula pelicula = new Pelicula("El club de la lucha", 90, 18, "David Fincher");
+		
+		JOptionPane.showMessageDialog(null, "TITULO: "+pelicula.getTitulo()
+		+"\nDURACION: "+pelicula.getDuracion()
+		+"\nEDAD MINIMA: "+pelicula.getEdadMinima()
+		+"\nDIRECTOR: "+pelicula.getDirector()
+		+"\n", "CINE", JOptionPane.INFORMATION_MESSAGE);
+	 
+		platea();
+		
+
+	}
+	
+
+    private static void alignamentJoption(String[][]arr, String chart) {
+    	
+        String output;        
+        StringBuilder sb = new StringBuilder();
+        sb.append("<html><table cellspacing=15>");
+        for (int i = 0; i < arr.length; i++) {
+            sb.append("<tr>");
+            for (int j = 0; j < arr.length + 1; j++) {
+                sb.append("<td>").append(arr[i][j]).append(" ").append("</td>");
+            }
+        }
+        sb.append("</tr></table></html>");
+        output = sb.toString();
+      	
+        JOptionPane.showMessageDialog(null, output, chart, JOptionPane.PLAIN_MESSAGE);
+       
+    }
+
+	public static void platea() {
+		
+		String[][]platea={{"1A","1B","1C","1D","1E","1F","1G","1H","1I"},
+						  {"2A","2B","2C","2D","2E","2F","2G","2H","2I"},
+						  {"3A","3B","3C","3D","3E","3F","3G","3H","3I"},
+						  {"4A","4B","4C","4D","4E","4F","4G","4H","4I"},
+						  {"5A","5B","5C","5D","5E","5F","5G","5H","5I"},
+						  {"6A","6B","6C","6D","6E","6F","6G","6H","6I"},
+						  {"7A","7B","7C","7D","7E","7F","7G","7H","7I"},
+						  {"8A","8B","8C","8D","8E","8F","8G","8H","8I"}};    
+		alignamentJoption(platea, "PLATEA");
+		alignamentJoption(randomizeButacas(platea), "BUTACAS DISPONIBLES");
+		
+	}
+	 public static String[][] randomizeButacas(String butacas[][]) {
+		 
+		 int butaca;
+		 if (butacas != null) {
+			 for(int i = 0; i<butacas.length; i++) {
+				 for (int j = 0; j < butacas[0].length; j++) {
+					 butaca = (int) ((Math.random() * (9 - 1)) + 1) - 1;
+					 if (butaca < 4)
+						 butacas[i][j] = "  ";
+			    	}			    	
+			  }		    
+		}
+		return butacas;
+		 
+
+		 
+	}
+
+	
+    
 }
