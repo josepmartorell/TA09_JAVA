cb02405c4d8bc79feb2fd52f9a416027ba219f9f
joelromero2911 <108723592+joelromero2911@users.noreply.github.com>
2022-07-17 13:02:55 +0200

    terminada 1a version funcional de TA06, integrada funcionalidad entre el main y Cine, y refactor de algunas funciones implicadas

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,6 +1,6 @@
 import javax.swing.JOptionPane;
 
-import cine_06.Pelicula;
+import cine_06.Cine;
 import electrodomestico_01.Lavadora;
 
 public class MainApp {
@@ -18,11 +18,11 @@ public class MainApp {
 	 * Menu inicial
 	 */
 	public static void initializeMenu() {
-		
+
 		// Mensaje con las diferentes opciones del menu
-		final String menuOpciones = "Elija una opci贸n." + "\n1 - Aplicaci贸n Electrodom茅stico"
-				+ "\n2 - Aplicaci贸n Serie" + "\n3 - Aplicaci贸n Libro" + "\n4 - Aplicaci贸n Raices"
-				+ "\n5 - Aplicaci贸n Aula POO" + "\n6 - Aplicaci贸n Cine" + "\n0 - SALIR";
+		final String menuOpciones = "Elija una opci贸n." + "\n1 - Aplicaci贸n Electrodom茅stico" + "\n2 - Aplicaci贸n Serie"
+				+ "\n3 - Aplicaci贸n Libro" + "\n4 - Aplicaci贸n Raices" + "\n5 - Aplicaci贸n Aula POO"
+				+ "\n6 - Aplicaci贸n Cine" + "\n0 - SALIR";
 		String opcion = "";
 
 		// Bucle infinito del menu hasta que se cierre con el Exit o cancelando
@@ -54,97 +54,13 @@ public class MainApp {
 				// TODO
 				break;
 			case "6":
-				salaProyeccion();
-				
+				Cine cine = new Cine();
+				cine.salaProyeccion();
 				break;
 			default:
 				JOptionPane.showMessageDialog(null, "Opci贸n incorrecta");
 			}
 
 		} while (!opcion.equals("0"));
-		
 	}
-	
-	/**
-	 * Metodo que muestra las caracter韘ticas de la pel韈ula en el cine
-	 */
-	public static void salaProyeccion() {
-		
-		// Creamos la pelicula
-		Pelicula pelicula = new Pelicula("El club de la lucha", 90, 18, "David Fincher");
-		
-		// Mostramos sus atributos
-		JOptionPane.showMessageDialog(null, "TITULO: "+pelicula.getTitulo()
-		+"\nDURACION: "+pelicula.getDuracion()
-		+"\nEDAD MINIMA: "+pelicula.getEdadMinima()
-		+"\nDIRECTOR: "+pelicula.getDirector()
-		+"\nCOSTE: 10�"
-		+"\n", "CINE", JOptionPane.INFORMATION_MESSAGE);
-	 
-		platea();
-		
-	}
-	
-
-	/**
-	 * Metodo que ...
-	 * 
-	 * @param arr
-	 * @param chart
-	 */
-    private static void alignamentJoption(String[][]arr, String chart) {
-    	
-        String output;        
-        StringBuilder sb = new StringBuilder();
-        sb.append("<html><table cellspacing=15>");
-        for (int i = 0; i < arr.length; i++) {
-            sb.append("<tr>");
-            for (int j = 0; j < arr.length + 1; j++) {
-                sb.append("<td>").append(arr[i][j]).append(" ").append("</td>");
-            }
-        }
-        sb.append("</tr></table></html>");
-        output = sb.toString();
-      	
-        JOptionPane.showMessageDialog(null, output, chart, JOptionPane.PLAIN_MESSAGE);
-       
-    }
-
-    /**
-     * Metodo que...
-     */
-	public static void platea() {
-		
-		String[][]platea={{"1A","1B","1C","1D","1E","1F","1G","1H","1I"},
-						  {"2A","2B","2C","2D","2E","2F","2G","2H","2I"},
-						  {"3A","3B","3C","3D","3E","3F","3G","3H","3I"},
-						  {"4A","4B","4C","4D","4E","4F","4G","4H","4I"},
-						  {"5A","5B","5C","5D","5E","5F","5G","5H","5I"},
-						  {"6A","6B","6C","6D","6E","6F","6G","6H","6I"},
-						  {"7A","7B","7C","7D","7E","7F","7G","7H","7I"},
-						  {"8A","8B","8C","8D","8E","8F","8G","8H","8I"}};    
-		alignamentJoption(platea, "PLATEA");
-		alignamentJoption(randomizeButacas(platea), "BUTACAS DISPONIBLES");
-	}
-	
-	/**
-	 * Metodo que...
-	 * 
-	 * @param butacas
-	 * @return
-	 */
-	public static String[][] randomizeButacas(String butacas[][]) {
-		 
-		 int butaca;
-		 if (butacas != null) {
-			 for(int i = 0; i<butacas.length; i++) {
-				 for (int j = 0; j < butacas[0].length; j++) {
-					 butaca = (int) ((Math.random() * (9 - 1)) + 1) - 1;
-					 if (butaca < 4)
-						 butacas[i][j] = "  ";
-			    	}			    	
-			  }		    
-		}
-		return butacas;	 
-	}    
 }
--- src/cine_06/Cine.java
+++ src/cine_06/Cine.java
@@ -5,42 +5,42 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-
 import javax.swing.JOptionPane;
 
 public class Cine {
-    private ArrayList<Butaca> butacas = new ArrayList<Butaca>();
-    private Pelicula pelicula;
-    private final float precioEntrada = 10F;
-    private ArrayList<Espectador> espectadores = new ArrayList<Espectador>();
+	private ArrayList<Butaca> butacas = new ArrayList<Butaca>();
+	private Pelicula pelicula;
+	private float precioEntrada;
+	private ArrayList<Espectador> espectadores = new ArrayList<Espectador>();
+	private int aforo;
 
-    /**
-     * Constructor con aforo y pelicula
-     * 
-	 * @param butacas
+	/**
+	 * Constructor con aforo y pelicula
+	 * 
 	 * @param pelicula
+	 * @param aforo
 	 */
-	public Cine(Pelicula pelicula, boolean aforo) {
+	public Cine() {
 		crearButacas();
-		this.pelicula = pelicula;
+		this.precioEntrada = 10F;
+		this.aforo = this.butacas.size();
 	}
 
-	
 	// Getters y setters
 
 	/**
 	 * @return butacas butacas
 	 */
-    public ArrayList<Butaca> getButacas() {
-        return butacas;
-    }
+	public ArrayList<Butaca> getButacas() {
+		return butacas;
+	}
 
-    /**
-     * @param butacas butacas
-     */
-    public void setButacas(ArrayList<Butaca> butacas) {
-        this.butacas = butacas;
-    }
+	/**
+	 * @param butacas butacas
+	 */
+	public void setButacas(ArrayList<Butaca> butacas) {
+		this.butacas = butacas;
+	}
 
 	/**
 	 * @return the pelicula
@@ -69,35 +69,34 @@ public class Cine {
 	public void setEspectadores(ArrayList<Espectador> espectadores) {
 		this.espectadores = espectadores;
 	}
-	
-	
-	
-	// Otros metodos
-	
-	/**
-	 * Metodo que añade todas las butacas a un arraylist de butacas (8 columnas * 9 filas = 72 butacas)
-	 */
-	public void crearButacas(){
-        String[] columnas = {"1", "2", "3", "4", "5", "6", "7", "8"};
-        String[] filas = {"A", "B", "C", "D", "E", "F", "G", "H", "I"};
 
-        // Recorremos el array de columnas y por cada posicion recorremos el de filas
-        for (int i = 0; i < columnas.length; i++) {
-            for (int j = 0; j < filas.length; j++) {
-                String nombre = columnas[i] + filas[j]; 
-                Butaca butaca = new Butaca(nombre, false);
-                this.butacas.add(butaca);
-            }
-        }
-    }
-	
+	// Otros metodos
+
+	/**
+	 * Metodo que aï¿½ade todas las butacas a un arraylist de butacas (8 columnas * 9
+	 * filas = 72 butacas)
+	 */
+	public void crearButacas() {
+		String[] columnas = { "1", "2", "3", "4", "5", "6", "7", "8" };
+		String[] filas = { "A", "B", "C", "D", "E", "F", "G", "H", "I" };
+
+		// Recorremos el array de columnas y por cada posicion recorremos el de filas
+		for (int i = 0; i < columnas.length; i++) {
+			for (int j = 0; j < filas.length; j++) {
+				String nombre = columnas[i] + filas[j];
+				Butaca butaca = new Butaca(nombre, false);
+				this.butacas.add(butaca);
+			}
+		}
+	}
+
 	/**
 	 * Metodo que crea "num" de espectadores con atributos aleatorios
 	 * 
 	 * @param num
 	 * @param cine
 	 */
-	public void crearEspectadoresAleatorios(int num, Cine cine) {
+	public void crearEspectadoresAleatorios(int num) {
 
 		final String chars = "abcdefghijklmnopqrstuvwxyz";
 
@@ -121,62 +120,158 @@ public class Cine {
 			this.espectadores.add(e);
 		}
 	}
-	
-	
+
 	/**
-	 * Este metodo asigna aleatoriamente espectadores a las butacas a partir del array de espectadores
+	 * Este metodo asigna aleatoriamente espectadores a las butacas a partir del
+	 * array de espectadores
 	 */
 	public void asignarEspectadoresAleatoriamente() {
-        Iterator<Espectador> itr = this.espectadores.iterator();
-        // Iteramos por cada espectador del contenedor espectadores
-        while(itr.hasNext()){
-            Espectador espectador = itr.next();
+		ArrayList<Espectador> espectadoresAptos = new ArrayList<Espectador>();
+		Iterator<Espectador> itr = this.espectadores.iterator();
+		int espectadoresSentados = 0;
 
-            if(validar(espectador.getEdad(), espectador.getDinero())) {
-            
-	            // Busca una butaca aleatoria, comprueba que no este ocupada, asigna la primera que encuentra libre
-	            int butaca = (int) ((Math.random() * (this.butacas.size() - 1)) + 1) - 1;
-	            while(this.butacas.get(butaca).isOcupado()) {
-	                butaca = (int) ((Math.random() * (this.butacas.size() - 1)) + 1) - 1;
-	            }
-	            
-	            // Butaca cambia a ocupada
-	            this.butacas.get(butaca).setOcupado(true);
-	            
-	            // Se asigna el nombre de la butaca a la variable butaca del espectador
-	            espectador.setButacaAsignada(this.butacas.get(butaca).getNombre());
-            }
-        }
-    }
+		// Iteramos por cada espectador del contenedor espectadores
+		while (itr.hasNext()) {
+			Espectador espectador = itr.next();
+
+			if (validar(espectador.getEdad(), espectador.getDinero()) && espectadoresSentados < this.aforo) {
+				// Incrementamos contador de espectadores sentados
+				espectadoresSentados++;
+
+				// Busca una butaca aleatoria, comprueba que no este ocupada, asigna la primera
+				// que encuentra libre
+				int butaca;
+				do {
+					butaca = (int) (Math.random() * ((this.butacas.size()) - 0)) + 0;
+				} while (this.butacas.get(butaca).isOcupado());
+
+				// Butaca cambia a ocupada
+				this.butacas.get(butaca).setOcupado(true);
+
+				// Se asigna el nombre de la butaca a la variable butaca del espectador
+				espectador.setButacaAsignada(this.butacas.get(butaca).getNombre());
+
+				espectadoresAptos.add(espectador);
+			}
+		}
+
+		this.espectadores = espectadoresAptos;
+	}
+
 	/**
-	 * Metodo que valida que exista localidad libre, la edad permitida para la pelicula y el dinero del espectador
+	 * Metodo que valida que exista localidad libre, la edad permitida para la
+	 * pelicula y el dinero del espectador
 	 * 
-	 * @param aforo
 	 * @param edad
 	 * @param dinero
 	 * @return
 	 */
 	public boolean validar(int edad, float dinero) {
 		// Si se cumplen los requisitos el espectador puede sentarse
-		if((this.pelicula.getEdadMinima() > edad) || (dinero < precioEntrada)) {	
+		if ((this.pelicula.getEdadMinima() > edad) || (dinero < precioEntrada)) {
 
-			return false;				
+			return false;
 		}
-		return true;	
+		return true;
 	}
 
 	/**
-	 * Método que muestra los espectadores y sus atributos en sus respectivas butacas
+	 * Mï¿½todo que muestra los espectadores y sus atributos en sus respectivas
+	 * butacas
 	 * 
 	 * @return
 	 */
 	public String mostrarEspectadores() {
-		// TODO POR PANTALLA espectadores, nombre, dinero, edad
-			ArrayList<Espectador> gente = this.getEspectadores();
-			JOptionPane.showMessageDialog(null, "ESPECTADORES: "+gente, null, JOptionPane.INFORMATION_MESSAGE);
-		
+		String infoGente = "";
+		Iterator<Espectador> itrEspectadores = this.espectadores.iterator();
+		while (itrEspectadores.hasNext()) {
+			Espectador espectador = itrEspectadores.next();
+			infoGente += "Nombre: " + espectador.getNombre() + " | Edad: " + espectador.getEdad() + " | Butaca: "
+					+ espectador.getButacaAsignada() + "\n";
+		}
+		JOptionPane.showMessageDialog(null, infoGente, "ESPECTADORES", JOptionPane.INFORMATION_MESSAGE);
+
 		return null;
 	}
-	
-	
+
+	/**
+	 * Metodo que muestra por pantalla todas las butacas y luego las disponibles
+	 */
+	public void mostrarPlatea() {
+
+		String[][] platea = { { "1A", "1B", "1C", "1D", "1E", "1F", "1G", "1H", "1I" },
+				{ "2A", "2B", "2C", "2D", "2E", "2F", "2G", "2H", "2I" },
+				{ "3A", "3B", "3C", "3D", "3E", "3F", "3G", "3H", "3I" },
+				{ "4A", "4B", "4C", "4D", "4E", "4F", "4G", "4H", "4I" },
+				{ "5A", "5B", "5C", "5D", "5E", "5F", "5G", "5H", "5I" },
+				{ "6A", "6B", "6C", "6D", "6E", "6F", "6G", "6H", "6I" },
+				{ "7A", "7B", "7C", "7D", "7E", "7F", "7G", "7H", "7I" },
+				{ "8A", "8B", "8C", "8D", "8E", "8F", "8G", "8H", "8I" } };
+		alignamentJoption(platea, "PLATEA");
+
+		String[][] plateaAsientosDisponibles = platea;
+		Iterator<Butaca> itrButaca = this.butacas.iterator();
+
+		for (int i = 0; i < platea.length; i++) {
+			for (int j = 0; j < platea[i].length; j++) {
+				Butaca butaca = itrButaca.next();
+				if (butaca.isOcupado()) {
+					plateaAsientosDisponibles[i][j] = "  ";
+				}
+			}
+		}
+		alignamentJoption(plateaAsientosDisponibles, "BUTACAS DISPONIBLES");
+	}
+
+	/**
+	 * Metodo que crea el JOptionPanel que la funcion mostrarPlatea() mostrara por
+	 * pantalla
+	 * 
+	 * @param arr
+	 * @param chart
+	 */
+	private void alignamentJoption(String[][] arr, String chart) {
+
+		String output;
+		StringBuilder sb = new StringBuilder();
+		sb.append("<html><table cellspacing=15>");
+		for (int i = 0; i < arr.length; i++) {
+			sb.append("<tr>");
+			for (int j = 0; j < arr.length + 1; j++) {
+				sb.append("<td>").append(arr[i][j]).append(" ").append("</td>");
+			}
+		}
+		sb.append("</tr></table></html>");
+		output = sb.toString();
+
+		JOptionPane.showMessageDialog(null, output, chart, JOptionPane.PLAIN_MESSAGE);
+
+	}
+
+	/**
+	 * Metodo que crea y muestra las caracterï¿½sticas de la pelï¿½cula en el cine,
+	 * pregunta cuanta gente intentara ver la pelicula, filtra a la gente entre los
+	 * que pueden ser o no espectadores, les asigna butaca y muestra toda la info
+	 * por pantalla
+	 */
+	public void salaProyeccion() {
+
+		// Creamos la pelicula
+		this.pelicula = new Pelicula("El club de la lucha", 90, 18, "David Fincher");
+
+		// Mostramos sus atributos
+		JOptionPane.showMessageDialog(null,
+				"TITULO: " + this.pelicula.getTitulo() + "\nDURACION: " + this.pelicula.getDuracion()
+						+ "\nEDAD MINIMA: " + this.pelicula.getEdadMinima() + "\nDIRECTOR: "
+						+ this.pelicula.getDirector() + "\nCOSTE: 10ï¿½" + "\n",
+				"CINE", JOptionPane.INFORMATION_MESSAGE);
+
+		int numEspectadores = Integer.parseInt(
+				JOptionPane.showInputDialog("Introduce el numero de espectadores que iran a ver la pelicula de culto "
+						+ pelicula.getTitulo() + " (max. aforo 72):"));
+		crearEspectadoresAleatorios(numEspectadores);
+		asignarEspectadoresAleatoriamente();
+		mostrarPlatea();
+		mostrarEspectadores();
+	}
 }
